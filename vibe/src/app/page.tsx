3d2020ab10dbb519999452c751ec22f1c5867353
thao-1 <tranminhthao5555@gmail.com>
2025-07-05 17:27:43 -0400

    Initial change

---
+++ vibe/src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+
+const Page = () => {
+    return (
+        <div>
+            <Button variant="new">
+              Click Me
+            </Button>
+        </div>
+    );
+}
+
+export default Page;
