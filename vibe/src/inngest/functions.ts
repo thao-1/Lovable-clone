0abe9715ce71b7cdfb1934ab0498408fda8670fe
thao-1 <tranminhthao5555@gmail.com>
2025-07-06 23:30:01 -0400

    Step 6: sandbox

--- vibe/src/inngest/functions.ts
+++ vibe/src/inngest/functions.ts
@@ -1,10 +1,18 @@
 import { openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
+import { Sandbox } from "@e2b/code-interpreter";
+import { stepsSchemas } from "inngest/api/schema";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }: { event: { data: { value: string } } }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("vibe-nextjs-thao");
+      return sandbox.sandboxId;
+    });
+      
     const summarizer = createAgent({
       name: "summarizer",
       system: "You are an expert summarizer. You summarize in 2 words.",
@@ -12,9 +20,16 @@ export const helloWorld = inngest.createFunction(
     });
 
     const output = await summarizer.run(
-      `Summarize the following text: ${event.data.value}`
+      `Summarize the following text: ${event.data.name}`
     );
 
-    return output;
-  }
+
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = await sandbox.getHost(3000);
+      return `http://${host}`;
+    })
+
+    return { output, sandboxUrl };
+  },
 );
